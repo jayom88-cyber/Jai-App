f4ddfe15291eeb9f37b8afc00eee0760808fe254
Jairo Bonilla <jayom88@gmail.com>
2026-07-19 13:56:24 -0400

    Baked CSS via NPM Script

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.{html,js}",
+    "./api/**/*.{html,js}",
+    "./jai-bridge/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
